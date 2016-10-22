c9fc2c54782ec779894410bc2b55ed8ef2aba720
jvutukur <jvutukur@asu.edu>
2016-10-22 12:32:05 -0700

    Added unit test cases for standalone program

---
+++ test/standalone/model/FetchLatestStockPricesTest.java
@@ -0,0 +1,35 @@
+package standalone.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import standalone.dao.Company;
+
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
+public class FetchLatestStockPricesTest {
+
+	 private static FetchLatestStockPrices ftsp;
+	
+	@BeforeClass
+	public  static void setUpBeforeClass() throws Exception {
+		ftsp =  new FetchLatestStockPrices();		
+	}
+
+	@Test
+	public void test() {
+		ArrayList<Company> list = ftsp.getCompaniesList();
+		Assert.assertEquals(true, list.size()>0);
+	}
+	
+	@Test
+	public void test_1() {
+		ArrayList<Company> list = ftsp.getCompaniesList();
+		Assert.assertEquals(true, list.size()>0);
+	}
+
+}
