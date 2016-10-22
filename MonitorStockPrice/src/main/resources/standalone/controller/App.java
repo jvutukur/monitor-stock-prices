17ace918c8655e33f41f26201d97d68e9e0436d5
jvutukur <jvutukur@asu.edu>
2016-10-22 01:08:20 -0700

    Created empty maven web app

---
+++ MonitorStockPrice/src/main/resources/standalone/controller/App.java
@@ -0,0 +1,24 @@
+package standalone.controller;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import model.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );        
+        //new CreateDB().createSchema();        
+        //AddStockValuesJob job = new AddStockValuesJob();
+        //job.startStockUpdateJob();
+        
+    }
+
+}
