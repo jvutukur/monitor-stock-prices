74ed5cfff4f642215f24e11b0ace2d108444e764
jvutukur <jvutukur@asu.edu>
2016-10-24 02:17:52 -0700

    added a sample unit test cases for restful api

--- test/rest/api/CrudControllerTest.java
+++ test/rest/api/CrudControllerTest.java
@@ -2,24 +2,28 @@ package rest.api;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.expect;
-import groovyx.net.http.ContentType;
+//import groovyx.net.http.ContentType;
+
+
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 
 public class CrudControllerTest {
 
-	
+	String getProd = "http://localhost:8080/MonitorStockPrice/rest/yahoostocks/companies_list";
     @Before
     public void setUp(){
-        RestAssured.basePath = "http://localhost:8080";
+        RestAssured.basePath = "";
     }
     
     @Test
     public void testGetProducts(){
-        expect().statusCode(200).contentType(ContentType.JSON).when()
-                .get("/yahoostocks/companies_list");
+        expect().statusCode(200).contentType("").when().get(getProd);
+        
     }
 
 }
